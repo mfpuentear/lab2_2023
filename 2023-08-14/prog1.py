f47f52983d09824273e8c846905d5aacce0efce6
Matias De la Puente <mfpuente.ar@gmail.com>
2023-10-23 16:08:14 -0300

    Clase del 2023-08-14

---
+++ 2023-08-14/prog1.py
@@ -0,0 +1,8 @@
+# Tupla
+
+tupla = ('a', 'palabra', 'mas')
+
+print(tupla[-2:])
+
+for t in tupla:
+  print (t)
---
+++ 2023-08-14/prog2.py
@@ -0,0 +1,9 @@
+def operaciones(a,b):
+  suma = a + b
+  resta = a - b
+  return suma, resta
+
+rsuma, rresta = operaciones(2,6)
+
+print("suma: ",rsuma)
+print("resta: ", rresta)
